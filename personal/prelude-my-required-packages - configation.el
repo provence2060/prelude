cfe6bc0d90d9247be6961410fe01b58248ec087c
provence2060 <provence2060@163.com>
2020-03-30 15:34:24 +0800

    Update prelude-my-required-packages - configation.el

--- personal/prelude-my-required-packages - configation.el
+++ personal/prelude-my-required-packages - configation.el
@@ -3,25 +3,6 @@
 ;;建议您避免更改个人文件夹之外的内容，以免将来不得不处理git merge冲突。
 
 
-;; LaTeX-mode后，添加xelatex编译选项
-;;;;(with-eval-after-load "tex"
-;;;;   (add-hook 'LaTeX-mode-hook
-;;;;           (lambda ()
-;;;;                (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
-;;;;                (setq TeX-command-default "XeLaTeX")
-;;;;                (setq TeX-save-query  nil )
-;;;;                (setq TeX-show-compilation t))))
-
-;; LaTeX-mode后，添加xelatex编译选项
-(with-eval-after-load "tex"
-   (add-hook 'LaTeX-mode-hook
-           (lambda ()
-                (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
-                (setq TeX-command-default "XeLaTeX")
-                (setq TeX-save-query  nil )
-                (setq TeX-show-compilation t))))
-
-
 
 ;;禁用和弦key-chords
 ;;在某些情况下，您可能不希望使用由序言定义的键和弦
