eba7e6aa33d6adf04ce3e6b486b6f730dcafe60c
provence2060 <provence2060@163.com>
2020-04-11 08:36:18 +0800

    Add files via upload

--- personal/prelude-my-required-packages - configation.el
+++ personal/prelude-my-required-packages - configation.el
@@ -1,8 +1,10 @@
 ;;https://prelude.emacsredux.com/en/latest/configuration/
 ;;分叉（而不是克隆）正式的Prelude回购，并为其添加您自己的风格。
 ;;建议您避免更改个人文件夹之外的内容，以免将来不得不处理git merge冲突。
+(with-eval-after-load 'flycheck
+  (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
-
+(global-auto-revert-mode t)
 
 ;;禁用和弦key-chords
 ;;在某些情况下，您可能不希望使用由序言定义的键和弦
